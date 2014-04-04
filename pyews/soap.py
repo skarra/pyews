9d84bd96d40f5621dc46bc8361df5aea45b655e1
Sriram Karra <karra.etc@gmail.com>
2014-04-04 16:46:39 +0530

    Misc chagnges to soap.py
    - new helper functions to parse xml responses
    - send nw throws a custom exception on connectiorn errors
    - misc. fixes

--- pyews/soap.py
+++ pyews/soap.py
@@ -23,10 +23,12 @@ import xml.etree.ElementTree as ET
 import utils
 
 M_NAMESPACE = 'http://schemas.microsoft.com/exchange/services/2006/messages'
+E_NAMESPACE = 'http://schemas.microsoft.com/exchange/services/2006/errors'
 T_NAMESPACE = 'http://schemas.microsoft.com/exchange/services/2006/types'
 
 def unQName (name):
-    return re.match('{.*)}(.*)', name).group(1)
+    res = re.match('{.*}(.*)', name)
+    return name if res is None else res.group(1)
 
 def QName (namespace, name):
     return '{%s}%s' % (namespace, name)
@@ -34,10 +36,16 @@ def QName (namespace, name):
 def QName_M (name):
     return QName(M_NAMESPACE, name)
 
+def QName_E (name):
+    return QName(E_NAMESPACE, name)
+
 def QName_T (name):
     return QName(T_NAMESPACE, name)
 
 class SoapMessageError(Exception):
+    pass
+
+class SoapConnectionError(Exception):
     pass
 
 class SoapClient(object):
@@ -48,13 +56,23 @@ class SoapClient(object):
         self.auth = HTTPBasicAuth(user, pwd)
 
     def send (self, request):
-        r = requests.post(self.url, auth=self.auth, data=request,
-                          headers={'Content-Type':'text/xml; charset=utf-8',
-                                   "Accept": "text/xml"})
+        """
+        Send the given rquest to the server, and return the response text as
+        well as a parsed node object as a (resp.text, node) tuple.
+
+        The response text is the raw xml including the soap headers and stuff.
+        """
+
+        try:
+            r = requests.post(self.url, auth=self.auth, data=request,
+                              headers={'Content-Type':'text/xml; charset=utf-8',
+                                       "Accept": "text/xml"})
+        except requests.exceptions.ConnectionError as e:
+            raise SoapConnectionError(e)
 
         r_code, node = SoapClient.get_response_code(r.text)
         if r_code != 'NoError':
-            r_msg, node = SoapClient.get_error_msg(r.text, node)
+            r_msg, node = SoapClient.get_error_msg(r.text, node, code=r_code)
             raise SoapMessageError(r_msg)
 
         return r.text, node
@@ -62,6 +80,44 @@ class SoapClient(object):
     @staticmethod
     def parse_xml (soap_resp):
         return ET.fromstring(soap_resp)
+
+    @staticmethod
+    def get_node_attribute (soap_resp, root, node, att):
+        """
+        From given soap response xml find and return the attributes of the
+        specified node (first occurrence)
+        """
+
+        if root is not None:
+            root = SoapClient.parse_xml(soap_resp)
+
+        for i in root.iter(QName_M(node)):
+            return (i.attrib[att], root) if i is not None else (None, root)
+
+        return (None, root)
+
+    @staticmethod
+    def get_node_detail (soap_resp, root, node):
+        """
+        From given soap response xml find the first occurrence of node and
+        return a tuple (node.text, node.attr, root)
+
+        node should be a string. root should be an Element object if not
+        None. In the returned tuple node.text willbe a string, node.attrib
+        will be a dictionary.
+        """
+
+        if root is not None:
+            root = SoapClient.parse_xml(soap_resp)
+
+        if root is None:
+            return (None, None, None)
+
+        for i in root.iter(node):
+            if i is not None:
+                return (i.text, i.attrib, root)
+
+        return (None, None, root)
 
     @staticmethod
     def get_response_code (soap_resp, root=None):
@@ -73,12 +129,32 @@ class SoapClient(object):
             root = SoapClient.parse_xml(soap_resp)
 
         for i in root.iter(QName_M('ResponseCode')):
-            return (i.text, root) if i is not None else (None, root)
+            if i is not None:
+                return (i.text, root)
+
+        for i in root.iter('faultcode'):
+            if i is not None:
+                ## hack alert: stripping out the namespace prefix...
+                return (i.text[2:], root)
+
+        for i in root.iter(QName_E('ResponseCode')):
+            if i is not None:
+                return (i.text, root)
+
+        return (None, root)
 
     @staticmethod
-    def get_error_msg (soap_resp, root=None):
+    def get_error_msg (soap_resp, root=None, code=None):
         if root is not None:
             root = SoapClient.parse_xml(soap_resp)
 
-        for i in root.iter(QName_M('MessageText')):
-            return (i.text, root) if i is not None else (None, root)
+        if code == 'ErrorSchemaValidation':
+            for i in root.iter('faultstring'):
+                if i is not None:
+                    return (i.text, root)
+        else:
+            for i in root.iter(QName_M('MessageText')):
+                if i is not None:
+                    return (i.text, root)
+
+        return (None, root)
